9d42a7dee8a3652c4a5af4a301c1b8690bcb5a01
liuhongtaowxp@163.com <lhl5227505>
2019-11-25 07:34:41 +0800

    dao封装

--- src/main/java/com/istonesoft/qdts/dao/QdConsumerDao.java
+++ src/main/java/com/istonesoft/qdts/dao/QdConsumerDao.java
@@ -3,12 +3,16 @@ package com.istonesoft.qdts.dao;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.istonesoft.qdts.context.QdContextHolder;
 import com.istonesoft.qdts.jdbc.QdJdbcTemplate;
 import com.istonesoft.qdts.jdbc.RowHandler;
 @Component
@@ -40,5 +44,31 @@ public class QdConsumerDao {
 				groupId, methodString,status,null,new Date(),0,desc	
 		});
 		
+	}
+	/**
+	 * 得到消费端调用时发生网络异常的数据
+	 * @return
+	 * @throws Exception
+	 * @throws SQLException
+	 */
+	public List<Map<String, String>> getNetWorkExceptionData() throws Exception, SQLException {
+		List<Map<String, String>> list = jdbcTemplate.selectList(ds, "select group_id, method from t_qd_consumer where status=? and (exception='netConnectException' or exception is null)", new Object[] {"PREPARE"}, new RowHandler<Map<String, String>>() {
+
+			public Map<String, String> handle(ResultSet rs) {
+				Map<String, String> map = new HashMap<String, String>();
+				
+				try {
+					map.put("groupId", rs.getString(1));
+					map.put("method", rs.getString(2));
+					return map;
+				} catch (SQLException e) {
+					QdContextHolder.getQdContext().clear();
+					e.printStackTrace();
+				}
+				return null;
+			}
+			
+		});
+		return list;
 	}
 }
--- src/main/java/com/istonesoft/qdts/schedule/ScheduleRunner.java
+++ src/main/java/com/istonesoft/qdts/schedule/ScheduleRunner.java
@@ -1,24 +1,16 @@
 package com.istonesoft.qdts.schedule;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import javax.sql.DataSource;
-
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
 import com.istonesoft.qdts.context.QdConsumerContext;
 import com.istonesoft.qdts.context.QdContextHolder;
-import com.istonesoft.qdts.jdbc.QdJdbcTemplate;
-import com.istonesoft.qdts.jdbc.RowHandler;
+import com.istonesoft.qdts.dao.QdConsumerDao;
 import com.istonesoft.qdts.resource.QdGroup;
 import com.istonesoft.qdts.resource.QdMethod;
 import com.istonesoft.qdts.resource.QdStatus;
@@ -26,18 +18,16 @@ import com.istonesoft.qdts.resource.QdStatus;
 @Component
 public class ScheduleRunner implements ApplicationContextAware {
 	@Autowired
-	private QdJdbcTemplate jdbcTemplate;
-	
-	@Autowired
-	protected DataSource ds;
+	private QdConsumerDao qdConsumerDao;
 	
 	private ApplicationContext applicationContext;
 	
 	@Scheduled(fixedDelay = 1000 * 3600)
 	public void invokeNetWorkExceptionData() {
+		
 		try {
 			//扫描调用超时的数据,状态为prepare,并且没有exception信息的数据
-			List<Map<String, String>> exceptionList = getNetWorkExceptionData();
+			List<Map<String, String>> exceptionList = qdConsumerDao.getNetWorkExceptionData();
 			for (Map<String, String> dataMap : exceptionList) {
 				//反序列化为QdMethod
 				QdMethod method = QdMethod.createQdMethodUseJson(dataMap.get("method"));
@@ -46,8 +36,9 @@ public class ScheduleRunner implements ApplicationContextAware {
 				method.invoke(this.applicationContext);
 			}
 		} catch (Exception e) {
-			QdContextHolder.getQdContext().clear();
 			e.printStackTrace();
+		} finally {
+			QdContextHolder.getQdContext().clear();
 		}
 		
 	}
@@ -62,32 +53,6 @@ public class ScheduleRunner implements ApplicationContextAware {
 		group.setMethod(method);
 		group.setStatus(QdStatus.PREPARE);
 		((QdConsumerContext)QdContextHolder.getQdContext(true)).setQdGroup(group);
-	}
-	/**
-	 * 得到消费端调用时发生网络异常的数据
-	 * @return
-	 * @throws Exception
-	 * @throws SQLException
-	 */
-	private List<Map<String, String>> getNetWorkExceptionData() throws Exception, SQLException {
-		List<Map<String, String>> list = jdbcTemplate.selectList(ds, "select group_id, method from t_qd_consumer where status=? and (exception='netConnectException' or exception is null)", new Object[] {"PREPARE"}, new RowHandler<Map<String, String>>() {
-
-			public Map<String, String> handle(ResultSet rs) {
-				Map<String, String> map = new HashMap<String, String>();
-				
-				try {
-					map.put("groupId", rs.getString(1));
-					map.put("method", rs.getString(2));
-					return map;
-				} catch (SQLException e) {
-					QdContextHolder.getQdContext().clear();
-					e.printStackTrace();
-				}
-				return null;
-			}
-			
-		});
-		return list;
 	}
 
 	@Override
